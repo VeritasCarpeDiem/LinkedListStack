dfcd3e2345dd8d0cc876f8ec5419ceb5b069bbc4
Jin Kim <Jin.Kim@GMR.local>
2020-07-17 12:31:12 -0700

    2nd commit

---
+++ Linked-List Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            for (int i = 0; i < 10; i++)
+            {
+                stack.Push(i.ToString());
+            }
+
+            stack.DisplayStack();
+
+            Console.WriteLine("----------------------------------");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(stack.Peek());
+                stack.Pop();
+            }
+
+            stack.DisplayStack();
+
+
+            //stack.Push("hello");
+
+            //Console.WriteLine(stack.Count);
+
+            //stack.Peek();
+
+            //stack.Push("world");
+
+            //stack.Peek();
+
+            //stack.DisplayStack();
+
+            //stack.Pop();
+
+            //Console.WriteLine(stack.Count);
+        }
+    }
+}
---
+++ Linked-List Stack/Stack.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace Linked_List_Stack
+{
+    // node containing strings
+    // stack of strings
+    public class Node<T>
+    {
+
+        public T Value { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node<T> Prev { get; set; }
+        public Node(T val)
+        {
+            this.Value = val;
+        }
+
+    }
+
+    public class Stack<T> //LIFO: Last In, First Out
+    {
+        public Node<T> Head { get; private set; }
+        public Node<T> Tail { get; private set; }
+
+        public int Count { get; private set; }
+        public Stack()
+        {
+            Count = 0;
+            Head = null;
+        }
+        public bool IsEmpty()
+        {
+            if (Head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        void AddLast(T Value)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T>(Value);
+                Tail = Head;
+            }
+            else
+            {
+                Node<T> newNode = new Node<T>(Value);
+
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+                Tail = newNode;
+            }
+
+
+            //if (IsEmpty())
+            //{
+            //    Head = new Node(Value);
+            //    Tail = Head;
+            //}
+            //else
+            //{
+            //    Tail.Next = new Node(Value);
+            //    Tail.Prev = Tail;
+            //    Tail = Tail.Next;
+                 
+            //}
+            Count++;
+        }
+
+        public void Push(T Value)
+        {
+            AddLast(Value);
+        }
+
+        void RemoveLast()
+        {
+           
+            if (!IsEmpty())
+            {
+                if (Tail == null)//case for 1 item
+                {
+                   
+                    Head = Tail;
+                    
+                   
+                }
+                
+                else
+                {
+                    Tail = Tail.Prev;
+                   // Tail.Next = null;
+                }
+                Count--;
+            }
+            
+        }
+        public void Pop()
+        {
+            RemoveLast();
+
+        }
+        public T Peek()
+        {
+            return Tail.Value;
+
+        }
+
+        public void DisplayStack()
+        {
+            Node<T> current = Head;
+            for (int i = 0; i < Count; i++)
+            {
+                Console.Write(current.Value + ",");
+                current = current.Next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
